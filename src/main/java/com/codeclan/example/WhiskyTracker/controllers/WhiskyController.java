c0aa4b5b94b06b0d1db1bf34fe326d7c871664b6
Christina Davies <christinahdavies@gmail.com>
2019-01-23 16:56:35 +0000

    added routes

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,31 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/whiskies")
 public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/region/{region}")
+    public List<Whisky> findWhiskyWithRegionNamed(@PathVariable String region){
+        return whiskyRepository.findWhiskyWithRegionNamed(region);
+    }
+
+    @GetMapping(value = "/distillery/age/{distilleryId}/{age}")
+    public List<Whisky> findWhiskyByDistilleryAndAge(@PathVariable Long distilleryId, @PathVariable int age){
+        return whiskyRepository.findWhiskyByDistilleryAndAge(distilleryId, age);
+    }
+
 
 }
